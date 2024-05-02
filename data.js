f487c0f7e27d8453eaa2aa06c26fb26f780ac3cb
srivarshini7 <srivarshinirao7@gmail.com>
2024-05-02 18:30:03 +0530

    intial commit

---
+++ data.js
@@ -0,0 +1,30 @@
+const articles = [
+    {
+      id: 1,
+      title: 'Jane Eyre by Charlotte Bronte',
+      date: new Date(2020, 9, 4),
+      length: 11,
+      snippet: `A gothic masterpiece of tempestuous passions and dark secrets, Charlotte Brontë's Jane Eyre is edited with an introduction and notes by Stevie Davis in Penguin Classics.`,
+    },
+    {
+      id: 2,
+      title: 'Wuthering Heights by Emily Bronte',
+      date: new Date(2019, 10, 22),
+      length: 5,
+      snippet: `This best-selling Norton Critical Edition is based on the 1847 first edition of the novel. For the Fourth Edition, the editor has collated the 1847 text with several modern editions and has corrected a number of variants, including accidentals.`,
+    },
+    {
+      id: 3,
+      title: 'Haunting of the Hill House by Shirley Jackson',
+      date: new Date(2018, 7, 11),
+      length: 5,
+      snippet: `It is the story of four seekers who arrive at a notoriously unfriendly pile called Hill House: Dr. Montague, an occult scholar looking for solid evidence of a "haunting".`,
+    },
+    {
+      id: 4,
+      title: 'Dracula by Bram Stoker',
+      date: new Date(2015, 5, 4),
+      length: 5,
+      snippet: `In Dracula, Bram Stoker created one of the great masterpieces of the horror genre, brilliantly evoking a nightmare world of vampires and vampire hunters and also illuminating the dark corners of Victorian sexuality and desire.`,
+    },
+  ];
